9495f577b5b8951942cfb4add1ddbd083ba6f04e
erclu <30255227+erclu@users.noreply.github.com>
2019-10-29 16:53:05 +0100

    feat: write cmdlet output in a powershellic (?) way

--- Scripts/Get-DirtyRepos.ps1
+++ Scripts/Get-DirtyRepos.ps1
@@ -3,18 +3,27 @@ param (
   [System.IO.DirectoryInfo]$RootFolder = $env:PROJECTS_FOLDER
 )
 
-Get-ChildItem -Force -Recurse $RootFolder -Include ".git" | ForEach-Object {
-  $gitDir = $_
-  $workTree = Split-Path -Path $_ -Parent
+Get-ChildItem -Force -Recurse $RootFolder -Include ".git" |
+  ForEach-Object {
+    $gitDir = $_
+    $workTree = Split-Path -Path $_ -Parent
 
-  $dirtyIndex = $null -ne (git.exe --git-dir $gitDir --work-tree $workTree status -s)
-  $unpushedCommits = $null -ne (git.exe --git-dir $gitDir --work-tree $workTree log --branches --not --remotes --oneline)
-  $forgottenStashes = $null -ne (git.exe --git-dir $gitDir stash list)
+    $dirtyIndex = $null -ne (git.exe --git-dir $gitDir --work-tree $workTree status -s)
+    $unpushedCommits = $null -ne (git.exe --git-dir $gitDir --work-tree $workTree log --branches --not --remotes --oneline)
+    $forgottenStashes = $null -ne (git.exe --git-dir $gitDir stash list)
 
-  if ($dirtyIndex -or $unpushedCommits -or $forgottenStashes) {
-    Write-Output "found local stuff in $workTree"
-  }
-  else {
-    Write-Output "all backed up in     $workTree"
-  }
-}
+    # if ($dirtyIndex -or $unpushedCommits -or $forgottenStashes) {
+    #   Write-Output "found local stuff in $workTree"
+    # }
+    # else {
+    #   Write-Output "all backed up in     $workTree"
+    # }
+    [pscustomobject]@{
+      Repo                = $workTree
+      AllGood             = -not ($dirtyIndex -or $unpushedCommits -or $forgottenStashes)
+      "Changes to commit" = $dirtyIndex
+      "Commits to push"   = $unpushedCommits
+      "Stashes to clear"  = $forgottenStashes
+    }
+  } |
+  Format-Table -Wrap
