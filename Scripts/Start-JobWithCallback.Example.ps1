9b3972363d6ace289e84afeb5040ea5b1ab3d3c3
erclu <30255227+erclu@users.noreply.github.com>
2019-11-04 13:54:46 +0100

    refactor: remove warnings from start-jobwithcallback

--- Scripts/Start-JobWithCallback.Example.ps1
+++ Scripts/Start-JobWithCallback.Example.ps1
@@ -1,25 +1,28 @@
 $job = Start-Job -Name "ExampleJob" -ScriptBlock {
-  Start-Sleep -Seconds 15
+  Start-Sleep -Seconds 5
   Write-Output "Hello from a job"
 }
 
 $cmdArgs = @{
-  # XXX this needs to be hardcoded inside the scriptblock
+  # XXX "EventHandlerExample" needs (??) to be hardcoded inside the scriptblock
   SourceIdentifier = "EventHandlerExample"
   InputObject      = $job
   EventName        = "StateChanged"
   Action           = {
     # Do things here
-    Write-Host "Hello from the handler"
+
+    # XXX Write-Output does not work here; the action runs as a job
+    # Write-Host "Hello from the handler"
 
     # Cleanup resources
     if ($Sender.HasMoreData) {
-      Write-Host "job $($Sender.Name) says:`n$(Receive-Job $Sender)"
+      # Write-Host "`njob $($Sender.Name) says:`n$(Receive-Job $Sender)"
+      # Write-Host -NoNewline (& prompt)
     }
     Remove-Job $Sender.Id
 
-    # XXX a job with this name remains listed by Get-Job cmdlet. why?
     Unregister-Event -SourceIdentifier "EventHandlerExample"
+    Remove-Job "EventHandlerExample"
   }
 }
 Register-ObjectEvent @cmdArgs
