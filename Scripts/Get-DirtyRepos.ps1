376ae50f282b85d8f8b5ab1bd6b3df40f5cce645
erclu <30255227+erclu@users.noreply.github.com>
2019-11-06 18:35:13 +0100

    docs: remove dead comments and add parameter help

--- Scripts/Get-DirtyRepos.ps1
+++ Scripts/Get-DirtyRepos.ps1
@@ -1,6 +1,8 @@
 [CmdletBinding()]
 param (
-  [System.IO.DirectoryInfo]$RootFolder = $env:PROJECTS_FOLDER
+  # Folder to search for dirty repositories
+  [System.IO.DirectoryInfo]
+  $RootFolder = $env:PROJECTS_FOLDER
 )
 
 Get-ChildItem -Force -Recurse $RootFolder -Include ".git" |
@@ -12,12 +14,6 @@ Get-ChildItem -Force -Recurse $RootFolder -Include ".git" |
     $unpushedCommits = $null -ne (git.exe --git-dir $gitDir --work-tree $workTree log --branches --not --remotes --oneline)
     $forgottenStashes = $null -ne (git.exe --git-dir $gitDir stash list)
 
-    # if ($dirtyIndex -or $unpushedCommits -or $forgottenStashes) {
-    #   Write-Output "found local stuff in $workTree"
-    # }
-    # else {
-    #   Write-Output "all backed up in     $workTree"
-    # }
     [pscustomobject]@{
       PSTypename          = "GitRepo"
       Repo                = $workTree
