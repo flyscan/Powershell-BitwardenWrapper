8d5a67796341b100ae9d2d23ef127aa9502c6f7d
erclu <30255227+erclu@users.noreply.github.com>
2019-12-03 14:09:03 +0100

    test: add tests for test-hassensitivewords

--- BitwardenWrapper/BitwardenWrapper.Tests.ps1
+++ BitwardenWrapper/BitwardenWrapper.Tests.ps1
@@ -2,11 +2,48 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.ps1'
 
-Import-Module $here/$sut
+Import-Module -Force $here/$sut
 
 Describe "BitwardenWrapper" {
-  #FIXME fix me
-  It "fails" -Pending {
-    $true | Should -Be $false
+
+  It "should test the module" -Pending {
+    # TODO
+  }
+}
+
+Describe "Test-HasSensitiveWords" {
+
+  Context "Edge cases" {
+
+    It "should throw when null parameters are given" {
+      { Test-HasSensitiveWords -InputString "" -SensitiveWords @() } | Should -Throw
+    }
+
+    It "should throw when inputstring is null" {
+      { Test-HasSensitiveWords -InputString "" -SensitiveWords "one", "two" } | Should -Throw
+    }
+
+    It "should throw when SensitiveWords is null" {
+      { Test-HasSensitiveWords -InputString "string" -SensitiveWords @() } | Should -Throw
+    }
+  }
+
+  Context "Normal Behaviour" {
+
+    It "returns false if input string is not in the array" {
+      Test-HasSensitiveWords -InputString "one" -SensitiveWords "two", "three" | Should -Be $false
+    }
+
+    It "returns true if input string is in the array" {
+      Test-HasSensitiveWords -InputString "one" -SensitiveWords "one", "two" | Should -Be $true
+    }
+
+    It "returns false if input string is a partial match for any of the array items" {
+      Test-HasSensitiveWords -InputString "on" -SensitiveWords "one", "two" | Should -Be $false
+    }
+
+    It "returns true if input string contains any of the array items" {
+      Test-HasSensitiveWords -InputString "bone" -SensitiveWords "one", "two" | Should -Be $true
+    }
   }
 }
